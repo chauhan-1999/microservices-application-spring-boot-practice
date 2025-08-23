015635b720e91b1f2f07fb8f9bc76efeb9f1b461
Chauhan-1999 <tarunchauhan798528@gmail.com>
2025-08-23 17:36:12 +0530

    added initial files service registry,department service and employee service

---
+++ service-registry/src/main/java/com/chauhan/service_registry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.chauhan.service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //This App will work as a service registry
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
